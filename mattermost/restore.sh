63c8976b3e58fb6758232e66c8cc3de07a1faa0d
Jan-Luca Klees <email@janlucaklees.de>
2023-01-26 21:05:11 +0100

    Added script for restoring mattermost
    The backup script is still in the making. I'll check it in as soon as I
    am confident it is working properly.
    
    I went back to using docker volumes as they are easier to handle and my
    backup strategy does not require any locally mounted files.

---
+++ mattermost/restore.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+
+################################################################################
+# PREPARATIONS                                                                 #
+################################################################################
+
+# Loading env variables
+. ./mattermost/.env
+
+# Stopping and removing everything that might still be running
+./manage.sh mattermost down -v
+
+
+################################################################################
+# FILES                                                                        #
+################################################################################
+
+# Create a fresh mattermost container
+./manage.sh mattermost create mattermost
+
+# Copying files into the mattermost container
+./manage.sh mattermost cp ./backup/config/config.json mattermost:/mattermost/config/config.json
+./manage.sh mattermost cp ./backup/data mattermost:/mattermost
+
+# Set the correct permissions
+./manage.sh mattermost run --rm --user root mattermost chown -R mattermost:mattermost /mattermost/config/config.json
+./manage.sh mattermost run --rm --user root mattermost chown -R mattermost:mattermost /mattermost/data
+
+
+################################################################################
+# DATABASE                                                                     #
+################################################################################
+
+# Start the database up again
+./manage.sh mattermost up -d database
+
+# Restore the database
+./manage.sh mattermost exec --no-TTY database pg_restore \
+  --role=${DATABASE_USER} \
+  --username=${DATABASE_USER} \
+  --dbname=${DATABASE_NAME} \
+  --format=tar \
+  --no-security-labels \
+  --no-owner \
+  --verbose \
+  < ./mattermost/backup/dump.sql
+
+
+################################################################################
+# Finalize                                                                     #
+################################################################################
+
+# Start everything up again
+./manage.sh mattermost up -d
