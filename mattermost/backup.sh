90ed19d6987b11ae24fd664fa69daa41968cd82a
Jan-Luca Klees <email@janlucaklees.de>
2023-02-23 09:37:47 +0100

    Added script for backing up mattermost

---
+++ mattermost/backup.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+
+################################################################################
+# PREPARATIONS                                                                 #
+################################################################################
+
+# Loading env variables
+. ./mattermost/.env
+
+# Delete the old backup to not mix any data
+rm -rf ./backup
+
+# Creating required folders
+mkdir -p ./backup
+mkdir -p ./backup/client
+
+
+################################################################################
+# FILES                                                                        #
+################################################################################
+
+# Copying files from inside the docker container
+./manage.sh mattermost cp mattermost:/mattermost/config/config.json ./backup/config/config.json
+./manage.sh mattermost cp mattermost:/mattermost/data ./backup
+
+
+################################################################################
+# DATABASE                                                                     #
+################################################################################
+
+# Backing up the Database
+./manage.sh exec --no-TTY database pg_dump \
+  --role=${DATABASE_USER} \
+	--username=${DATABASE_USER} \
+	--dbname=${DATABASE_NAME} \
+	--format=tar \
+	--no-security-labels \
+  --no-owner \
+  --verbose \
+	> ./backup/dump.sql
